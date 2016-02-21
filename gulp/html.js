73ca62b573af61c189dbdc45660a07d2192675eb
Eric Quanz <eric.quanz@gmail.com>
2016-02-21 16:05:12 -0300

    use gulp-file-include to handle partials, split index file

--- gulp/html.js
+++ gulp/html.js
@@ -1,10 +1,12 @@
 'use strict';
 
 var gulp       = require('gulp'),
+    include    = require('gulp-include'),
     minifyHtml = require('gulp-htmlmin');
 
 gulp.task('html', function() {
-  return gulp.src('src/*.html')
+  return gulp.src(['src/*.html','src/include/*.html'])
+    .pipe(include())
     .pipe(minifyHtml({
       collapseWhitespace: true
     }))
