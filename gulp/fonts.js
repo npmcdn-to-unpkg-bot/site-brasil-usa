a965ec2b2eb71999e80ea0b6b3f42847984be951
Eric Quanz <eric.quanz@gmail.com>
2016-03-06 15:37:45 -0300

    add webfonts

---
+++ gulp/fonts.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var gulp = require('gulp');
+
+gulp.task('fonts', function() {
+  return gulp.src('src/fonts/**')
+    .pipe(gulp.dest('dist/fonts/'));
+});
--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,10 @@ require('./gulp/serve');
 require('./gulp/scripts');
 require('./gulp/styles');
 require('./gulp/images');
+require('./gulp/fonts');
 require('./gulp/html');
 require('./gulp/clean');
 
-gulp.task('build', ['html', 'css', 'javascript', 'images']);
+gulp.task('build', ['html', 'css', 'javascript', 'images', 'fonts']);
 gulp.task('build:production', ['build', 'clean', 'css:production', 'javascript:production']);
 gulp.task('default', ['serve']);
