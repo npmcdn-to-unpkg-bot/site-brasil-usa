be7865e181a9c09a46c5ac6567edf9853870fe18
Eric Quanz <eric.quanz@gmail.com>
2016-03-06 22:30:06 -0300

    finish header for wide screen devices

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,45 @@
+/*eslint-disable no-console */
+
 'use strict';
 
 require('./track-js-errors')();
 
+var $ = function(selector) {
+    var selectorType = 'querySelectorAll';
 
-(function ($) {
-  'use strict';
+    if (selector.indexOf('#') === 0) {
+        selectorType = 'getElementById';
+        selector = selector.substr(1, selector.length);
+    }
+
+    return document[selectorType](selector);
+};
+
+function toggleClass(el, className) {
+  if (el.classList) {
+    el.classList.toggle(className);
+  } else {
+    var classes = el.className.split(' ');
+    var existingIndex = classes.indexOf(className);
+
+    if (existingIndex >= 0)
+      classes.splice(existingIndex, 1);
+    else
+      classes.push(className);
+
+    el.className = classes.join(' ');
+  }
+}
+
+var $menuBtn = $('#open-menu');
+
+$menuBtn.addEventListener('click', function toggleMenu() {
+  toggleClass(this, 'is-active');
+  toggleClass(document.documentElement, 'is-menu-open');
+});
+
+
 
   $('.js-toggle-form').on('change', function() {
     $('.js-togglable-form').toggleClass('sr-only');
   });
-} (jQuery));
